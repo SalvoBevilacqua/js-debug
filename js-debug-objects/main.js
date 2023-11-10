b5773d7566470516c7b85babedaad97de44a2904
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-10 16:41:35 +0100

    add:debug-object

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -11,6 +11,7 @@
 
 
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+/*
 const cars = [
     {
         manufacturer: 'Ford',
@@ -73,6 +74,84 @@ const dieselCars = cars.filter( (auto) => {
 const otherCars = cars.filter( (auto) => {
     return auto.type !== 'benzina' || auto.type !== 'diesel';
 });
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
+il codice vuole creare degli array di oggetti che soddisfino le condizioni date dai filtri.
+avere quindi un array con tutte le auto a benzina, uno con quelle diesel ed uno con tutte le altre.
+manca una virgola fra alcuni oggetti interni all'array.
+la prima arrow function è scritta in maniera errata.
+nel secondo filtro non serve la graffa.
+nel terzo filtro per avere "tutte le altre" devo usare AND e non OR
+*/
+
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (auto) => auto.type === 'benzina');
+
+const dieselCars = cars.filter( (auto) => auto.type === 'diesel');
+
+const otherCars = cars.filter( (auto) => (auto.type !== 'benzina' && auto.type !== 'diesel'));
 
 console.log('Auto a benzina');
 console.log('*******************************');
